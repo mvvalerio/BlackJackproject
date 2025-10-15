a998e3264491acdd5cacd45a7eb7c913f8347345
mvvalerio <martimvv07@gmail.com>
2025-10-15 14:16:20 +0000

    Done

--- maingame.py
+++ maingame.py
@@ -1,4 +1,24 @@
 import pygame
+import random
 import sys
-import os
 
+class Bank:
+
+class Carta:
+
+class Baralho:
+
+class Mao:
+
+class Player:
+
+class Dealer(Player):
+
+class Botoes:
+
+class Jogo:
+
+def main():
+
+if __name__ == "__main__":
+    main()
